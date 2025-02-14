75bfb3dd5669876f01640f598259b599bf14afd1
edpedro <edp2013.ep@gmail.com>
2025-02-13 21:21:03 -0300

    :chore: adicionando roles da rotas de permissão

--- src/permissions/controller/permissions.controller.ts
+++ src/permissions/controller/permissions.controller.ts
@@ -12,6 +12,7 @@ import { PermissionService } from '../service/permissions.service';
 import { CreatePermissionDto } from '../dto/create-permission.dto';
 import { UpdatePermissionDto } from '../dto/update-permission.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { Roles } from 'src/auth/decorators/roles.decorator';
 
 @Controller('permissions')
 @UseGuards(AuthGuard('jwt'))
@@ -19,21 +20,25 @@ export class PermissionsController {
   constructor(private readonly permissionsService: PermissionService) {}
 
   @Post()
+  @Roles('permission_create')
   create(@Body() createPermissionDto: CreatePermissionDto) {
     return this.permissionsService.create(createPermissionDto);
   }
 
   @Get()
+  @Roles('permission_read')
   findAll() {
     return this.permissionsService.findAll();
   }
 
   @Get(':id')
+  @Roles('permission_read')
   findOne(@Param('id') id: string) {
     return this.permissionsService.findOne(+id);
   }
 
   @Patch(':id')
+  @Roles('permission_update')
   update(
     @Param('id') id: string,
     @Body() updatePermissionDto: UpdatePermissionDto,
@@ -42,6 +47,7 @@ export class PermissionsController {
   }
 
   @Delete(':id')
+  @Roles('permission_delete')
   remove(@Param('id') id: string) {
     return this.permissionsService.remove(+id);
   }
